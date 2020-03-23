882409deb00cde7320f381192659032be348895d
the-jezza <sava_101@hotmail.com>
2020-03-23 16:44:50 +0000

    setting hours per week and salary

--- work_hours.rb
+++ work_hours.rb
@@ -1,1 +1,23 @@
 # Ruby script where you can add your yearly salary and tells you how much you earn per hour
+
+def working_days_year
+    working_days = 254
+end
+
+def work_hours_week
+    puts "How many hours do you work per week?"
+    week_hours = gets.chomp.to_i
+    salary(week_hours)
+end
+
+def salary(week_hours)
+    puts "What is your yearly salary?"
+    salary = gets.chomp.to_i
+end
+
+def num_weeks
+end
+
+work_hours_week
+# salary / 52 = x
+# x / weekly hours = per hour rate
